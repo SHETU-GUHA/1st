96267d065c79fdd0a02ac16c00cd26c2cba0b249
SHETU-GUHA <47523990+SHETU-GUHA@users.noreply.github.com>
2019-02-11 17:45:48 +0600

    Add files via upload

---
+++ 1st chapter.py
@@ -0,0 +1,33 @@
+print('Hellow World!')
+print ('What is your name?')
+myName = input()
+print ('it is good to meet you ' + myName)
+print('The length of our name is :')
+print (len(myName))
+print ('what is your age?')
+myAge = input ()
+print ('You will be ' + str (int(myAge)+1) + 'in a year.')
+
+
+#1. operator (*, -, / , +) values ('hello', -88.8, 5)
+# variable (spam),  string ('spam')
+# int,floating point ,string
+#  after running the code bacon value is 21
+# 'spam' + 'spamspam' = spamspamspam
+'spam' * 3 =
+spam
+spam
+spam
+
+#Why is eggs a valid variable name while 100 is invalid?
+   Because variable names cannot begin with a number.
+
+
+# What three functions can be used to get the integer, floating-point number, or string version of a value?
+   str()
+   int()
+   float()
+
+#Why does this expression cause an error? How can you fix it?
+   'I have eaten ' + 99 + ' burritos.'
+ This expression causes an error because here'I have eaten' and 'burritos' are strings, while 99 is treated as integer. In order to fix the error and print 'I have eaten 99 burritos.', 99 needs '' around it to treat it as a string.       
